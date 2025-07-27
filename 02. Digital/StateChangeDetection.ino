40a9a5752e24dd577d6eccbc57666a9a2c82c210
aravindsubra <125004417@sastra.ac.in>
2025-07-27 15:15:11 +0530

    Create StateChangeDetection.ino

---
+++ 02. Digital/StateChangeDetection.ino
@@ -0,0 +1,34 @@
+int laststatebutton = 0;
+int pushbuttoncount = 0;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT);
+}
+void loop()
+{
+  int buttonstate = digitalRead(2);
+  if(buttonstate != lastbuttonstate)
+  {
+    if(buttonstate == HIGH)
+    {
+      pushbuttoncount++;
+      Serial.println("No. of push button counts : ");
+      Serial.println(pushbuttoncount);
+    }
+    else
+    {
+      Serial.println("off");
+    }
+    lastbuttonstate = buttonstate;
+  }
+  if(pushbuttoncount%4 == 0)
+  {
+    digitalWrite(13, HIGH);
+  }
+  else
+  {
+    digitalWrite(13, LOW);
+  }
+}
