35ceea232884ab16adbcb40b29f5280b021cc526
aravindsubra <125004417@sastra.ac.in>
2025-08-02 07:30:03 +0530

    Create PhysicalPixel.ino

---
+++ 04. Communication/PhysicalPixel.ino
@@ -0,0 +1,17 @@
+const int ledpin = 13;
+int incomingbyte;
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledpin, OUTPUT);
+}
+void loop() {
+  if(Serial.Available() > 0) {
+    incomingbyte = Serial.read();
+    if(incomingbyte == 'H') {
+      digitalWrite(ledpin, HIGH);
+    }
+    if(incomingbyte == 'L') {
+      digitalWrite(ledpin, LOW);
+    }
+  }
+}
